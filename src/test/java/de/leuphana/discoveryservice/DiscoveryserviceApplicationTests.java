980956a80f1ed1323df338d3613f1c8b628938ed
kevinmoisch <kevinmoisch@gmail.com>
2020-03-08 14:36:26 +0100

    Initial Commit

---
+++ src/test/java/de/leuphana/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.leuphana.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
